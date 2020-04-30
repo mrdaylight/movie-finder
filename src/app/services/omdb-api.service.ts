6f237b2ee869d2a7a6a213e64545f551e7e66937
mrdaylight <mrdaylight@protonmail.com>
2020-04-30 02:42:48 +0100

    updating

---
+++ src/app/services/omdb-api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse } from '@angular/common//http';
+import { Observable} from 'rxjs';
+import { catchError, tap} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OmdbApiService {
+
+	private _siteURL='https://www.omdbapi.com/';
+	private _key='?apikey=47ebcb6d&t=';	
+
+  constructor(private _http:HttpClient) { }
+
+  getMovieData(movieName: string): Observable<IOMDBResponse> { {
+	  return this._http.get<IOMDBResponse>(this._siteURL + this._key + movieName)
+	  .pipe(
+		  tap(data => console.log('Moviedata/error' + JSON.stringify(data))
+		     ),
+		     catchError(this.handleError)
+	  );
+  }
+  }
+}
---
+++ src/interfaces/IOMDBResponse.ts
@@ -0,0 +1,6 @@
+interface IOMDBResponse {
+ Title: string;
+ Year: string;
+ Poster: string;
+ Genre: string;
+}
